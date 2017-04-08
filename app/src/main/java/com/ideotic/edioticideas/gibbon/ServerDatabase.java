efb2d6c8f04314f4cde25362ab41d148a77d1b31
Mukul Tiwari <Mukul Tiwari>
2017-04-08 14:47:51 +0530

    Initial commit

---
+++ app/src/main/java/com/ideotic/edioticideas/gibbon/ServerDatabase.java
@@ -0,0 +1,11 @@
+package com.ideotic.edioticideas.gibbon;
+
+/**
+ * Created by Mukul on 07-04-2017.
+ */
+public class ServerDatabase {
+
+    public boolean validate(String userName, String password){
+        return true;
+    }
+}
